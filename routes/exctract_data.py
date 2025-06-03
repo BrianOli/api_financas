8b5ee89e7847f004ccda2a1442fc7559baf48a0a
BrianOli <brianoliveira.contato@gmail.com>
2025-06-02 23:35:33 -0300

    Extract description and send to an AI LLM (NotFinish)

--- routes/exctract_data.py
+++ routes/exctract_data.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, jsonify, request
+from dotenv import load_dotenv
 import pandas as pd
+import requests
 import io
-import pdb
+import os
+
+load_dotenv()
 
 upload_bp = Blueprint('upload', __name__, url_prefix='/upload')
 
@@ -16,18 +20,11 @@ def upload_file():
 
     try:
         if uploaded_file and uploaded_file.filename.endswith('.csv'):
-            csv_content = uploaded_file.stream.read().decode('utf-8')
-            csv_file_buffer = io.StringIO(csv_content)
-            df = pd.read_csv(csv_file_buffer)
-
-            df_cols = [i for i in df.keys()]
-            columns_needed = ['Data', 'Valor', 'Descrição']
-            miss_cols = []
-
-            miss_cols = [col for col in columns_needed if col not in df_cols]
-            if miss_cols: return jsonify({'Columns missing': miss_cols}), 400
+            df = extract_data(uploaded_file)
             
-            df = df[columns_needed]
+            for data in df.Descrição:
+                data = summarize_desc(data)
+
             return jsonify({'message': 'CSV file processed successfully', 'data': df.to_dict(orient='records')}), 200
 
         else:
@@ -36,3 +33,37 @@ def upload_file():
 
     except Exception as e:
         return jsonify({'error': f'Server error: {str(e)}'}), 500
+
+def extract_data(uploaded_file):
+    csv_content = uploaded_file.stream.read().decode('utf-8')
+    csv_file_buffer = io.StringIO(csv_content)
+    df = pd.read_csv(csv_file_buffer)
+
+    df_cols = [i for i in df.keys()]
+    columns_needed = ['Data', 'Valor', 'Descrição']
+    miss_cols = []
+
+    miss_cols = [col for col in columns_needed if col not in df_cols]
+    if miss_cols: return jsonify({'Columns missing': miss_cols}), 400
+    
+    return df[columns_needed]
+
+
+def summarize_desc(desc):
+    prompt = f'Resuma: {desc}, Por exemplo: `Transferência Recebida - Adriano de Jesus Pereira Carvalho - •••.584.408-•• - NU PAGAMENTOS - IP (0260) Agência: 1 Conta: 32993603-6` transforme em `Transferência Recebida - Adriano de Jesus Pereira Carvalho`'
+
+    headers = {"Authorization": os.getenv('LLM_TOKEN')}
+    url = os.getenv('LLM_URL')
+    payload = {
+        "providers": "microsoft,connexun",
+        "language": "pt-br",
+        "text": f"{prompt}",
+    }
+
+    try:
+        response = requests.post(url, json=payload, headers=headers)    
+        result = response.json()
+        return result
+
+    except Exception as e:
+        return jsonify({'Server Error': f'{str(e)}'})
